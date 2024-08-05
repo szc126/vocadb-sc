ce1a44fa91eea01ba64051971b4dfb1e1e063eb0
szc126 <29127915+szc126@users.noreply.github.com>
2024-08-05 00:17:36 -0700

    initialize userscripts

---
+++ aimbot.user.js
@@ -0,0 +1,222 @@
+// ==UserScript==
+// @name        VocaDB aimbot 2024
+// @homepageURL https://github.com/szc126/vocadb-sc
+// @downloadURL https://raw.githubusercontent.com/szc126/vocadb-sc/main/aimbot.user.js
+// @namespace   szc
+// @match       *://www.nicovideo.jp/user/*
+// @match       *://www.nicovideo.jp/mylist/*
+// @match       *://www.nicovideo.jp/search/*
+// @match       *://www.nicovideo.jp/tag/*
+// @match       *://www.nicolog.jp/user/*
+// @match       *://www.youtube.com/*/videos
+// @match       *://www.youtube.com/*/search
+// @match       *://www.youtube.com/hashtag/*
+// @match       *://www.youtube.com/watch?*
+// @match       *://www.youtube.com/results?*
+// @match       *://space.bilibili.com/*
+// @match       *://search.bilibili.com/*
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @description for extreme gamers only
+// ==/UserScript==
+
+// shoutout to ChatGPT. i do not understand async/await
+// also. does the timeout cancel out the benefit (async) of using fetch(). lol.
+
+// domains: domains corresponding to each set of instructions (the stuff below)
+// a_query_selectors: CSS selectors for PV links
+// button_parent: the element that will contain the link to the VocaDB entry, defined relative to a PV link
+// nav_query_selectors: CSS selectors for the elements that will contain the link to start this script
+
+var server = false;
+(async () => {
+	server = await GM.getValue('server', 'vocadb.net');
+})();
+
+var services = {
+	'NicoNicoDouga': {
+		'domains': ['www.nicovideo.jp'],
+		'a_query_selectors': [
+			'a.NC-MediaObject-contents', // user
+			'.itemTitle a', // search & tag
+		],
+		'button_parent': function(a) {
+			return a.classList.contains('NC-Link') ? a.parentNode : a.parentNode.parentNode;
+		},
+		'nav_query_selectors': [
+			'.VideoContainer-headerTotalCount', // user
+			'.MylistHeader-name', // mylist
+			'.contentsBox[data-search-option] .toolbar', // search & tag
+		],
+	},
+	'NicoNicoDouga-Nicolog': {
+		'domains': ['www.nicolog.jp'],
+		'a_query_selectors': [
+			'.text-center a',
+		],
+		'button_parent': function(a) {
+			return a.parentNode.parentNode.nextElementSibling;
+		},
+		'nav_query_selectors': [
+			'th:first-of-type',
+		],
+	},
+	'Youtube': {
+		// NOTE:
+		// For channels,
+		// giving the all videos "Videos" playlist to https://vocadb.net/SongList/Import
+		// is faster;
+		// however, that will always be "newest"-first
+
+		// also if you spam this on lists of non-vocaloid videos
+		// i explode you with hammers
+		// TODO: refuse to process further after N unregistered videos
+
+		'domains': ['www.youtube.com'],
+		'a_query_selectors': [
+			'h3.ytd-rich-grid-media a', // channel and hashtag; the .ytd-rich-grid-media is necessary; user comments are h3.ytd-comment-view-model
+			'a.ytd-compact-video-renderer', // recommended sidebar
+			'a#video-title', // channel search
+		],
+		'button_parent': function(a) {
+			return a.parentNode;
+		},
+		'nav_query_selectors': [
+			//'#chips-content', // channel, "sort by" menu // channels with few videos do not have the "sort by" menu
+			'.yt-tab-group-shape-wiz__tabs', // channel, "home" "videos" "shorts" tab bar
+			'#offer-module', // recommended sidebar
+			'#center', // /results
+		],
+	},
+	'Bilibili': {
+		'domains': ['bilibili.com'],
+		'a_query_selectors': [
+			'.cube-list a.title', // space.bilibili.com
+			'.bili-video-card__info--right > a', // search.bilibili.com
+			'a.cover-normal', // space.bilibili.com collectiondetail
+		],
+		'button_parent': function(a) {
+			return a.parentNode;
+		},
+		'nav_query_selectors': [
+			'.contribution-list', // space.bilibili.com 投稿
+			'.search-nav', // space.bilibili.com 搜索
+			'.page-head', // space.bilibili.com collectiondetail
+			'.vui_tabs--nav', // search.bilibili.com
+		],
+	},
+}
+
+async function process_urls(service) { // ASYNC
+	let as = document.querySelectorAll(services[service].a_query_selectors);
+	for (let a of as) {
+		let url = a.href;
+
+		// allow running the script multiple times in one session.
+		// but do not look up our own links ofc
+		if (url.indexOf(server) > 0) {
+			continue;
+		};
+
+		// allow running the script multiple times in one session.
+		// skip songs that we have already found
+		if ('vocadbSongEntryId' in a.dataset) {
+			continue;
+		}
+
+		// skip ad links (the video ID is not embedded in the link)
+		if (url.indexOf('api.nicoad.nicovideo.jp') > 0) {
+			continue;
+		};
+
+		// TODO: destroy the old "create an entry link" when running the script again?
+
+		url = url.replace('nicolog.jp', 'nicovideo.jp');
+
+		let song_entry = await get_song_entry(url); // AWAIT
+		let button = create_button(url, song_entry);
+		services[service].button_parent(a).appendChild(button);
+		if (song_entry) {
+			a.dataset.vocadbSongEntryId = song_entry.id;
+		}
+	}
+}
+
+async function get_song_entry(url) { // ASYNC
+	let data_cached = await GM.getValue(url);
+	if (data_cached) {
+		return data_cached;
+	}
+
+	await new Promise(resolve => setTimeout(resolve, 2000)); // AWAIT
+
+	return fetch('https://' + server + '/api/songs?' + new URLSearchParams({
+		'query': url,
+		'fields': 'Tags',
+	})).then(response => response.json()
+	).then(data => {
+		if (data.items.length > 1) {
+			prompt('Notice: Found multiple song entries for:', url);
+		}
+		if (data.items.length > 0) {
+			GM.setValue(url, data.items[0]);
+
+			return data.items[0];
+		}
+	});
+
+	return false;
+}
+
+function create_button(url, song_entry) {
+	// using <a> instead of <button>,
+	// so that i can open multiple links at once
+	// using extensions like Snap Links
+	let a = document.createElement('a');
+	a.style.background = song_entry ? 'lime' : 'magenta';
+	a.style.padding = '0.5em';
+	a.href = song_entry ? 'https://' + server + '/S/' + song_entry.id : 'https://' + server + '/Song/Create?' + new URLSearchParams({
+		'pvUrl': url,
+	});
+	a.title = song_entry ? [song_entry.name, song_entry.songType, song_entry.artistString, song_entry.tags.map(tag => tag.tag.name).join(', ')].join('\n') : '';
+
+	let text = document.createTextNode(server);
+	a.appendChild(text);
+
+	return a;
+}
+
+function add_main_button(service) {
+	let navs = document.querySelectorAll(services[service].nav_query_selectors);
+	for (let nav of navs) {
+		// need to make a new button each time, yes
+
+		let button = document.createElement('button');
+		button.addEventListener('click', function(event) {
+			process_urls(service);
+		});
+		button.style.background = 'cyan';
+
+		let text = document.createTextNode('Scan for ' + server);
+		button.appendChild(text);
+
+		nav.appendChild(button);
+	}
+	// TODO: stop the script if clicked while running
+	// TODO: what happens if you click it while it's running, anyway
+}
+
+function main() {
+	for (let service in services) {
+		let domains = services[service].domains;
+		if (domains.some(domain => window.location.href.includes(domain))) {
+			add_main_button(service);
+		}
+	}
+}
+
+// add 2-second delay to let whatever fancy javascript framework finish rendering
+setTimeout(function() {
+	//alert('vocadb song add script is ready');
+	main();
+}, 2000);
---
+++ tag_preset.user.js
@@ -0,0 +1,145 @@
+// ==UserScript==
+// @name VocaDB tag presets
+// @homepageURL https://github.com/szc126/vocadb-sc
+// @downloadURL https://raw.githubusercontent.com/szc126/vocadb-sc/main/tag_preset.user.js
+// @namespace szc
+// @match https://vocadb.net/*
+// @match https://beta.vocadb.net/*
+// @grant none
+// ==/UserScript==
+
+const observer = new MutationObserver(mutations => {
+	mutations.forEach(record => {
+		record.addedNodes.forEach(node => {
+			if (node.id === 'nprogress') {
+				try {
+					document.getElementById('mytagspreset').remove();
+				} catch(error) {
+					0;
+				}
+				if (window.location.pathname.indexOf('/S/') === 0 || window.location.pathname.indexOf('/Song/') === 0) {
+					main();
+					//observer.disconnect();
+				}
+			}
+		});
+	});
+});
+observer.observe(document.body, {
+	subtree: true,
+	childList: true,
+});
+
+function main() {
+	let div = document.createElement("div");
+	div.id = 'mytagspreset'
+	div.classList.add("btn-group");
+	div.classList.add("navbar-languageBar");
+	div.style.display = 'flex';
+	div.style.flexWrap = 'wrap';
+	div.style.fontSize = '1em'; // goes with the line way down below
+	document.getElementsByClassName("sidebar-nav")[0].append(div);
+
+	const sets = [
+		["3D原", 3193, 3282],
+		["A", 22], // anime
+		["G", 455], // video game
+		["TV", 6715], // television
+		["M", 4677], // movie
+
+		["跨語", 7059],
+		["換語", 6224],
+		["填詞", 2866],
+		["パ", 330], // parody
+
+		["自翻", 391],
+		["自混", 392],
+		["ｼｮｰﾄ", 4717],
+		["ﾌﾙ", 3068],
+
+		["清唱", 7],
+		["晒し", 6302],
+		["ust配布", 3153],
+		["mp3配布", 160],
+		["ｵｹ配布", 3113],
+		["vb demo", 89],
+		["utau配布", 5027],
+		["sv跨語", 8639],
+		["svﾗｲﾄ", 8044],
+		["詰合pv", 6769],
+		["ai絵", 9119],
+		["公式絵", 6495],
+		["言葉遊", 6856],
+
+		["???", 7339, 6571],
+	];
+
+	const song_id = window.location.pathname.split('/').pop();
+
+	fetch(
+		'/api/users/current/songTags/' + song_id,
+		{
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json; charset=utf-8',
+			},
+		}
+	).then(response => response.json()
+	).then(data_old => {
+
+		for (let i = 0; i < sets.length; i++) {
+			let b = document.createElement("a");
+			b.innerHTML = sets[i][0];
+			b.addEventListener("click", function(event) {
+				let payload = [];
+				for (let j = 0; j < sets[i].slice(1).length; j++) {
+					payload.push({
+						"id": sets[i].slice(1)[j],
+					});
+				}
+				fetch(
+					'/api/users/current/songTags/' + song_id,
+					{
+						method: 'GET',
+						headers: {
+							'Content-Type': 'application/json; charset=utf-8',
+						},
+					}
+				).then(response => response.json()
+				).then(data => {
+					fetch(
+						'/api/users/current/songTags/' + song_id,
+						{
+							method: 'PUT',
+							headers: {
+								'Content-Type': 'application/json; charset=utf-8',
+							},
+							body: JSON.stringify(data.filter(tag => tag.selected).map(tag => tag.tag).concat(payload)),
+						}
+					).then(response => {
+						if (response.status >= 200 && response.status < 300) {
+							b.classList.remove('btn-default');
+							b.classList.add('btn-success');
+						}
+					})
+				});
+			});
+			b.classList.add("btn");
+			b.classList.add("btn-default");
+			b.style.fontSize = '125%'; // goes with the line way up above
+
+			/* ChatGPT start */
+			let tags_added = sets[i].slice(1).every(tag_id =>
+				data_old.some(item => item.tag.id === tag_id)
+			);
+			if (tags_added) {
+				b.classList.remove('btn-default');
+				b.classList.add('btn-success');
+			}
+			/* ChatGPT end */
+
+			div.append(b);
+		}
+
+	});
+}
